636b4fe8cada3378a4169d6601850f5c4443c15c
mandarkathe <mandar909@gmail.com>
2021-03-03 12:59:48 -0500

    demo

---
+++ app.py
@@ -0,0 +1,50 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import altair as alt
+from vega_datasets import data
+
+
+# Read in global data
+cars = data.cars()
+
+# Setup app and layout/frontend
+app = dash.Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+app.layout = html.Div([
+    html.Iframe(
+        id='scatter',
+        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+    dcc.Dropdown(
+        id='xcol-widget',
+        value='Horsepower',  # REQUIRED to show the plot on the first page load
+        options=[{'label': col, 'value': col} for col in cars.columns])])
+
+# Set up callbacks/backend
+@app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('xcol-widget', 'value'))
+def plot_altair(xcol):
+    chart = alt.Chart(cars).mark_point().encode(
+        x=xcol,
+        y='Displacement',
+        tooltip='Horsepower').interactive()
+    return chart.to_html()
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
